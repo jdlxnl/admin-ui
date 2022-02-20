02d87c025f75d712a3c639eed6d6582f8dbdf08b
Jeroen de Lau <jeroen.delau@gmail.com>
2022-02-20 11:38:42 +0100

    🎉 first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+Add the package by loading it through composer.
+
+
+```shell
+composer require jdlxnl/admin-ui
+```
---
+++ src/Provider/JdlxAdminUIServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jdlx\AdminUi;
+
+
+use Illuminate\Support\ServiceProvider;
+use Jdlx\AdminUi\Commands\AdminUi\ComponentScaffoldAdminUICommand;
+use Jdlx\AdminUi\Commands\AdminUi\GenerateCrudAdminUICommand;
+use Jdlx\AdminUi\Commands\AdminUi\PageScaffoldAdminUICommand;
+use Jdlx\AdminUi\Commands\AdminUi\RouterScaffoldAdminUICommand;
+
+class JdlxAdminUIServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
